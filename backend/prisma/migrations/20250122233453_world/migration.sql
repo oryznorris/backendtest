8c729b80b18eeb376595a8082091ece05a89b738
lucasdlftec <lucas.alexandre@dlftec.com.br>
2025-01-24 10:49:47 -0300

    Delete backend directory

--- backend/prisma/migrations/20250122233453_world/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `titulo` VARCHAR(191) NULL,
-    `descricao` VARCHAR(191) NULL,
-    `status` ENUM('PENDENTE', 'CONCLUIDO') NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
